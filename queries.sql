48c7480a4cfe1ac0947d47eb97e7b0b36c858ce6
vy3191 <venkyafhs@gmail.com>
2020-03-01 18:17:11 -0500

    Completed query#6

--- queries.sql
+++ queries.sql
@@ -22,7 +22,9 @@ WHERE "City" in ('London', 'Berlin', 'Madrid');
 INSERT INTO "Customers"("CustomerName", "ContactName", "Address", "City", "PostalCode", "Country")
 VALUES ("The Shire",  "Bilbo Baggins", "1 Hobbit-Hole", "Bag End", "111", "Middle Earth");
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-
+UPDATE "Customers"
+SET "PostalCode" = '11122'
+WHERE "ContactName" = 'Bilbo Baggins';
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
