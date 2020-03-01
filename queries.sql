b9639761a24a93c880c17b91b99f876f22f8560d
vy3191 <venkyafhs@gmail.com>
2020-03-01 17:59:40 -0500

    Completed query#3

--- queries.sql
+++ queries.sql
@@ -10,7 +10,10 @@ SELECT "Phone"
 FROM "Suppliers"
 WHERE "SupplierID" = "11";
 -- List first 10 orders placed, sorted descending by the order date
-
+SELECT *
+FROM "Orders"
+ORDER BY "OrderDate" DESC
+LIMIT 10;
 -- Find all customers that live in London, Madrid, or Brazil
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
